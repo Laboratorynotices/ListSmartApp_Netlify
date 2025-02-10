8a1447d9b1fac4bcfd17d861237c7879dd63ef67
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-02-10 19:40:09 +0100

    feat(config): добавлен модуль VueFire и обновлены зависимости Firebase
    - Установлены зависимости: vuefire и firebase (CSR и SSR)
    - Обновлены файлы `package.json` и `package-lock.json`.
    - В файл `nuxt.config.ts` добавлено использование модуля VueFire.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["nuxt-vuefire"],
+  vuefire: {
+    // Будем пользоваться авторизацией
+    auth: {
+      enabled: true,
+    },
+    // Подключение для CSR авторизации
+    config: {
+      apiKey: process.env.GOOGLE_FIREBASE_CONFIG_API_KEY,
+      authDomain: process.env.GOOGLE_FIREBASE_CONFIG_AUTH_DOMAIN,
+      projectId: process.env.GOOGLE_FIREBASE_CONFIG_PROJECT_ID,
+      storageBucket: process.env.GOOGLE_FIREBASE_CONFIG_STORAGE_BUCKET,
+      messagingSenderId: process.env.GOOGLE_FIREBASE_CONFIG_MESSAGING_SENDER_ID,
+      appId: process.env.GOOGLE_FIREBASE_CONFIG_APP_ID,
+    },
+  },
+});
